b4618d73840b2cb1ddc0d949bbdc9441297c0d8e
Sadique <sadique.ali@gmail.com>
2020-08-15 04:53:24 +0530

    removing unwanted file

--- consoleredirector.cpp
+++
@@ -1,9 +0,0 @@
-#include "consoleredirector.h"
-
-ConsoleRedirector::ConsoleRedirector(const string &outFile, bool autoReset)
-    : m_sOutFile(outFile), m_AutoReset(autoReset) {
-
-  FILE *fp = freopen(outFile.c_str(), "w", stdout);
-  if (fp) {
-  }
-}
